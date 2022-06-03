7f058bbe05d6afa74aed2a0ddd094eb6347da040
King-Francisco <106827926+King-Francisco@users.noreply.github.com>
2022-06-03 10:02:33 -0700

    Add files via upload

---
+++ My code1.py
@@ -0,0 +1,93 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+wait_time = random.randint(1,60)
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    wait_time = random.randint(1,60)
+NameError: name 'random' is not defined
+wait_time = random.randint(1, 60)
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    wait_time = random.randint(1, 60)
+NameError: name 'random' is not defined
+imprt random
+SyntaxError: invalid syntax
+import random
+wait_time = random.randint(1,60)
+wait_time
+11
+wait_time=random.randint(1,60)
+wait_time
+11
+wait_time
+11
+wait_time=random.randint(1,60)
+wait_time
+24
+wait_time=random.randint(1,60)
+wait_time
+15
+
+vowels=('a','e','i','o','u')
+word="Milliways"
+for letter in word:
+    if letter in vowels:
+        print(letter)
+
+        
+i
+i
+a
+
+=================== RESTART: C:/Users/USER/Desktop/vowels.py ===================
+i
+i
+a
+
+found = []
+len(found)
+0
+found.append('a')
+len(found)
+1
+found.append('e')
+found.append('i')
+found.append('o')
+len(found)
+4
+found
+['a', 'e', 'i', 'o']
+if 'u' not in found:
+    found.append('u')
+    found
+
+    
+['a', 'e', 'i', 'o', 'u']
+if 'u' not in found:
+    found.append('u')
+found
+SyntaxError: invalid syntax
+if 'u' not in found:
+    found.append('u')
+    found
+
+    
+
+if 'u' not in found:
+    found.append('u')
+found
+SyntaxError: invalid syntax
+
+=================== RESTART: C:/Users/USER/Desktop/vowels2.py ==================
+Provide a word to search for vowels:Blessings
+e
+i
+
+=================== RESTART: C:/Users/USER/Desktop/vowels2.py ==================
+Provide a word to search for vowels:Sea-bender
+e
+a
+
+=================== RESTART: C:/Users/USER/Desktop/vowels2.py ==================
+Provide a word to search for vowels:Sky
+
